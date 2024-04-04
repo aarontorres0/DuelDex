390b8856da4245829f3ecf8f0bec4c8093bab4f1
Aaron Torres <aarontorres1423@gmail.com>
2024-04-04 11:03:24 -0500

    feat: Enhance search functionality and UX

--- src/App.jsx
+++ src/App.jsx
@@ -26,55 +26,70 @@ function App() {
     }
   };
 
-  useEffect(() => {
-    const fetchRandomCards = async () => {
-      let fetchedCards = [];
-      for (let i = 0; i < 8; i++) {
-        try {
-          const response = await fetch(
-            "https://db.ygoprodeck.com/api/v7/randomcard.php"
-          );
-          const data = await response.json();
-          fetchedCards.push(data);
-        } catch (error) {
-          console.error("Error fetching random card:", error);
-        }
-      }
-      setCards(fetchedCards);
-    };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      searchCards();
+    }
+  };
 
+  const fetchRandomCards = async () => {
+    let fetchedCards = [];
+    for (let i = 0; i < 8; i++) {
+      try {
+        const response = await fetch(
+          "https://db.ygoprodeck.com/api/v7/randomcard.php"
+        );
+        const data = await response.json();
+        fetchedCards.push(data);
+      } catch (error) {
+        console.error("Error fetching random card:", error);
+      }
+    }
+    setCards(fetchedCards);
+  };
+
+  const resetApp = () => {
+    setSearchQuery("");
+    fetchRandomCards();
+  };
+
+  useEffect(() => {
     fetchRandomCards();
   }, []);
 
   return (
-    <div className="px-4 py-2">
-      <input
-        type="text"
-        placeholder="Search for a card"
-        value={searchQuery}
-        onChange={handleSearchChange}
-        className="mb-4"
-      />
-
-      <button
-        onClick={searchCards}
-        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
-      >
-        Search
-      </button>
-
+    <div className="container mx-auto px-4 py-2">
+      <div className="text-left">
+        <h1
+          className="text-2xl font-bold m-4 cursor-pointer"
+          onClick={resetApp}
+        >
+          DuelDex
+        </h1>
+      </div>
+      <div className="flex justify-center my-4">
+        <input
+          type="search"
+          placeholder="Search for a card"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          onKeyDown={handleKeyDown}
+          className="input input-bordered input-primary w-full max-w-xs"
+        />
+        <button onClick={searchCards} className="btn btn-primary ml-2">
+          Search
+        </button>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
         {cards.map((card, index) => (
-          <div
-            key={index}
-            className="flex-shrink-0 mr-4"
-            style={{ width: "240px" }}
-          >
-            <img
-              src={card.card_images[0].image_url}
-              alt={card.name}
-              style={{ width: "240px", height: "320px" }}
-            />
+          <div key={index} className="bg-base-100">
+            <figure className="bg-white">
+              <img
+                src={card.card_images[0].image_url}
+                alt={card.name}
+                className="w-full"
+              />
+            </figure>
           </div>
         ))}
       </div>
