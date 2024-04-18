896580ace86b374e920d340d813e4f0389a6f2f6
Aaron Torres <aarontorres1423@gmail.com>
2024-04-18 12:48:25 -0500

    fix: persist user session

--- AuthContext.jsx
+++ AuthContext.jsx
@@ -1,9 +1,25 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { supabase } from "./src/supabaseClient";
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const session = supabase.auth.getSession();
+    setUser(session?.user || null);
+
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (_, session) => {
+        setUser(session?.user || null);
+      }
+    );
+
+    return () => {
+      authListener.subscription.unsubscribe();
+    };
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
