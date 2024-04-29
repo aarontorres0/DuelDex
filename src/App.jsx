64e9a8ec8e4973da621261b971a022dd62dc2421
Aaron Torres <aarontorres1423@gmail.com>
2024-04-29 08:30:33 -0500

    feat: Clear text button inside search input

--- src/App.jsx
+++ src/App.jsx
@@ -84,14 +84,35 @@ const App = () => {
   return (
     <div className="container mx-auto px-4 py-2">
       <div className="flex justify-center my-4">
-        <input
-          type="search"
-          placeholder="Search for a card"
-          value={searchQuery}
-          onChange={handleSearchChange}
-          onKeyDown={handleKeyDown}
-          className="input input-bordered input-primary w-full max-w-xs"
-        />
+        <label className="relative w-full max-w-sm">
+          <input
+            type="search"
+            placeholder="Search for a card"
+            value={searchQuery}
+            onChange={handleSearchChange}
+            onKeyDown={handleKeyDown}
+            className="input input-bordered input-primary w-full max-w-sm"
+          />
+          {searchQuery.length > 0 && (
+            <button
+              onClick={() => setSearchQuery("")}
+              className="absolute inset-y-0 right-0 flex items-center p-2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-11.293a1 1 0 010 1.414L11.414 10l2.293 2.293a1 1 0 01-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 01-1.414-1.414L8.586 10 6.293 7.707a1 1 0 111.414-1.414L10 8.586l2.293-2.293a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          )}
+        </label>
         <button onClick={searchCards} className="btn btn-primary ml-2">
           Search
         </button>
